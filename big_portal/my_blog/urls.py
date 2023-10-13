d1fdb4bdddc423c52551c6877654a9dcbeb833e2
Waldemar <dnestrgroup@gmail.com>
2023-10-13 17:20:41 +0300

    Add routes views models and run migrations

--- big_portal/my_blog/urls.py
+++ big_portal/my_blog/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
+from .views import home, test_page
+# We import function home - from file my_blog/views.py
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('my_blog.urls'))
+    path('', home),
+    path('test/', test_page),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- big_portal/my_blog/views.py
+++ big_portal/my_blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Home Page</h1>')
+
+def test_page(request):
+    return HttpResponse('<h1>Test Page</h1>')
